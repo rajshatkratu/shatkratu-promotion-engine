7f3ad341603922b415afa13245b136c932f5ac04
shatkratu <raj.shatkratu@gmail.com>
2020-07-10 21:20:00 +0530

    SRG: Added Product class

---
+++ SRGPromotionEngine/SRGPromotionEngine.PromotionApp/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRGPromotionEngine.PromotionApp
+{
+    public class Product
+    {
+        public string Id { get; set; }
+    }
+}
