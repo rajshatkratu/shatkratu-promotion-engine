7d59f3a52f221933275cb4e90ecddd64fe897abb
shatkratu <raj.shatkratu@gmail.com>
2020-07-10 21:51:04 +0530

    SRG: Added new test cases for checking after applying promotion result is as expected or not  example 2

--- SRGPromotionEngine/SRGPromotionEngine.PromotionApp.UnitTests/PromotionEngineTests.cs
+++ SRGPromotionEngine/SRGPromotionEngine.PromotionApp.UnitTests/PromotionEngineTests.cs
@@ -228,5 +228,97 @@ namespace SRGPromotionEngine.PromotionApp.UnitTests
             Assert.That(result, Is.EqualTo(100));
 
         }
+
+        [Test]
+        public void ApplyPromotionToGetTotalSum_AfterApplyingValidPromotion_ReturnExpectedSum_Example2()
+        {
+            // Arrange
+            Dictionary<string, double> pinfo1 = new Dictionary<string, double>();
+            pinfo1.Add("A", 3);
+            Dictionary<string, double> pinfo2 = new Dictionary<string, double>();
+            pinfo2.Add("B", 2);
+            Dictionary<string, double> pinfo3 = new Dictionary<string, double>();
+            pinfo3.Add("C", 1);
+            pinfo3.Add("D", 1);
+
+            List<Promotion> promotions = new List<Promotion>()
+            {
+                new Promotion(1, pinfo1, 130),
+                new Promotion(2, pinfo2, 45),
+                new Promotion(3, pinfo3, 30)
+            };
+
+            List<Order> orders = new List<Order>();
+
+            orders.AddRange(new Order[] {
+                new Order()
+                {
+                    OrderId =1,
+                    Products = new List<Product>
+                    {
+                        new Product
+                        {
+                            Id = "A"
+                        },
+                        new Product
+                        {
+                            Id = "B"
+                        },
+                        new Product
+                        {
+                            Id = "C"
+                        }
+                    }
+                },
+                 new Order()
+                {
+                    OrderId =2,
+                    Products = new List<Product>
+                    {
+                        new Product
+                        {
+                            Id = "A"
+                        },
+                        new Product
+                        {
+                            Id = "A"
+                        },
+                        new Product
+                        {
+                            Id = "A"
+                        },
+                        new Product
+                        {
+                            Id = "A"
+                        },
+                        new Product
+                        {
+                            Id = "B"
+                        },
+                        new Product
+                        {
+                            Id = "B"
+                        },
+                        new Product
+                        {
+                            Id = "B"
+                        },
+                        new Product
+                        {
+                            Id = "B"
+                        }
+                    }
+                }
+            });
+
+            // Act 
+            decimal result = PromotionEngine.ApplyPromotionToGetTotalSum(orders, promotions, _productPricePairs);
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(370));
+
+        }
+
     }
 }
