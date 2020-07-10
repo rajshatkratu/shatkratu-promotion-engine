2045759f614adf7701427d317f579391c6258fd9
shatkratu <raj.shatkratu@gmail.com>
2020-07-10 21:36:36 +0530

    SRG: Included the common product id and corresponding price info as an Initial setup for test project

--- SRGPromotionEngine/SRGPromotionEngine.PromotionApp.UnitTests/PromotionEngineTests.cs
+++ SRGPromotionEngine/SRGPromotionEngine.PromotionApp.UnitTests/PromotionEngineTests.cs
@@ -8,5 +8,16 @@ namespace SRGPromotionEngine.PromotionApp.UnitTests
     [TestFixture]
     public class PromotionEngineTests
     {
+        private Dictionary<string, int> _productPricePairs;
+        [SetUp]
+        public void SetUp()
+        {
+            this._productPricePairs = new Dictionary<string, int>();
+
+            _productPricePairs.Add("A", 50);
+            _productPricePairs.Add("B", 30);
+            _productPricePairs.Add("C", 20);
+            _productPricePairs.Add("D", 15);
+        }
     }
 }
