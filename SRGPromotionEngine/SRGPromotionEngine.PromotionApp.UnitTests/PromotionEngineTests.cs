7b60fa95ccd7ea1ef8b62ac8ea8c61aec060df4f
shatkratu <raj.shatkratu@gmail.com>
2020-07-10 21:40:53 +0530

    SRG :  Blackbox testing :  Added 2 test cases to returns zero if orders argument is null or empty

--- SRGPromotionEngine/SRGPromotionEngine.PromotionApp.UnitTests/PromotionEngineTests.cs
+++ SRGPromotionEngine/SRGPromotionEngine.PromotionApp.UnitTests/PromotionEngineTests.cs
@@ -19,5 +19,63 @@ namespace SRGPromotionEngine.PromotionApp.UnitTests
             _productPricePairs.Add("C", 20);
             _productPricePairs.Add("D", 15);
         }
+
+        [Test]
+        public void ApplyPromotionToGetTotalSum_OrdersArgumentIsNull_ReturnZero()
+        {
+
+            // Arrange
+            Dictionary<string, double> pinfo1 = new Dictionary<string, double>();
+            pinfo1.Add("A", 3);
+            Dictionary<string, double> pinfo2 = new Dictionary<string, double>();
+            pinfo2.Add("B", 2);
+            Dictionary<string, double> pinfo3 = new Dictionary<string, double>();
+            pinfo3.Add("C", 2);
+
+            List<Promotion> promotions = new List<Promotion>()
+            {
+                new Promotion(1, pinfo1, 130),
+                new Promotion(2, pinfo2, 45),
+                new Promotion(3, pinfo3, 30)
+            };
+
+
+            // Act
+            decimal result = PromotionEngine.ApplyPromotionToGetTotalSum(null, promotions, _productPricePairs);
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(0));
+
+        }
+
+        [Test]
+        public void ApplyPromotionToGetTotalSum_OrdersArgumentIsEmpty_ReturnZero()
+        {
+
+            // Arrange
+            Dictionary<string, double> pinfo1 = new Dictionary<string, double>();
+            pinfo1.Add("A", 3);
+            Dictionary<string, double> pinfo2 = new Dictionary<string, double>();
+            pinfo2.Add("B", 2);
+            Dictionary<string, double> pinfo3 = new Dictionary<string, double>();
+            pinfo3.Add("C", 2);
+
+            List<Promotion> promotions = new List<Promotion>()
+            {
+                new Promotion(1, pinfo1, 130),
+                new Promotion(2, pinfo2, 45),
+                new Promotion(3, pinfo3, 30)
+            };
+
+
+            // Act
+            decimal result = PromotionEngine.ApplyPromotionToGetTotalSum(new List<Order>(), promotions, _productPricePairs);
+
+            // Assert
+
+            Assert.That(result, Is.EqualTo(0));
+
+        }
     }
 }
