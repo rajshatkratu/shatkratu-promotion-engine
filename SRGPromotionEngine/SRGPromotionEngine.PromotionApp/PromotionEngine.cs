20f705563cdb3e5f5ff65d9970d03cd3f0c561fa
shatkratu <raj.shatkratu@gmail.com>
2020-07-10 21:30:05 +0530

    SRG :  Added a method stub for ApplyPromotionToGetTotalSum in promotion class

--- SRGPromotionEngine/SRGPromotionEngine.PromotionApp/PromotionEngine.cs
+++ SRGPromotionEngine/SRGPromotionEngine.PromotionApp/PromotionEngine.cs
@@ -6,5 +6,16 @@ namespace SRGPromotionEngine.PromotionApp
 {
     public class PromotionEngine
     {
+        /// <summary>
+        /// Apply Promotion To Get TotalSum
+        /// </summary>
+        /// <param name="orders">orders containing order item</param>
+        /// <param name="promotions">promotion rules</param>
+        /// <param name="productPricePairs">price for a product as key value pairs</param>
+        /// <returns>total sum price of the orders after applying promotion</returns>
+        public static decimal ApplyPromotionToGetTotalSum(List<Order> orders, List<Promotion> promotions, Dictionary<string, int> productPricePairs)
+        {
+            return 0m;
+        }
     }
 }
